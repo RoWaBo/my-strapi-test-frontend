5ee63bb70db4f88a714c8bd8a5129c43ca4f6ce0
Robert <u1501851@rts-365.dk>
2021-12-23 23:23:37 +0100

    Created and added flexDirection fnc depending on even or odd index number

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,33 @@ import Image from "next/image";
 import qs from 'qs'
 import { getStrapiMedia, getStrapiURL } from '../helperFunctions';
 import { Article } from '../types/collectionTypes';
+import ReactMarkdown from 'react-markdown'
 
 interface HomeProps {
   articles: Article[]
 }
 
 const Home: NextPage<HomeProps> = ({ articles }) => {
+
+  const flexDirection = (value: number) => {
+    const isEven = value % 2 === 0 ? true : false
+    return isEven ? '' : 'flex-row-reverse';
+  }
+
   return (
     <>
       {console.log(articles)}
-      {articles.map((article) => (
-        <article key={article.id} className='mb-16'>
-          <div>
+      {articles.map((article, index) => (
+        <article key={article.id} className={`mb-10 md:mb-20 md:flex gap-14 ${flexDirection(index)}`}>
+          {/* TEXT SECTION */}
+          <div className='flex-1'>
             <h2 className='mb-4'>{article.attributes.title}</h2>
-            <p>{article.attributes.description}</p>
+            <ReactMarkdown className='react-markdown'>{article.attributes.description}</ReactMarkdown>
           </div>
+          {/* IMAGE SECTION */}
           {article.attributes.image?.data !== null && (
-            <div className='mt-4'>
-              <div className="relative w-full h-80 bg-gray-200 rounded-lg overflow-hidden">
+            <div className='mt-6 md:mt-1 flex-1'>
+              <div className="relative w-full h-80 md:h-full bg-gray-200 rounded-lg overflow-hidden shadow-lg">
                 {article.attributes.image?.data.attributes.url !== undefined && (
                   <Image
                     src={getStrapiMedia(article.attributes.image?.data.attributes.url)}
